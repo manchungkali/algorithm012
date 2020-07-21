285b2dd36556340e45197ea518f9d9cf0fe99608
manchungkali <cmkangsong@126.com>
2020-07-21 09:22:40 +0800

    day16

---
+++ Week_03/_05jianzhi_ti-huan-kong-ge-lcof.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String replaceSpace(String s) {
+        StringBuilder res = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c == ' ') res.append("%20");
+            else res.append(c);
+        }
+        return res.toString();
+
+    }
+
+
+}
