0183c4810cdec7afdd902fd06a3e69107a420e37
manchungkali <cmkangsong@126.com>
2020-07-30 22:07:35 +0800

    homework_week04

--- Week_04/jump-game.java
+++ Week_04/jump-game.java
@@ -4,6 +4,9 @@ class Solution {
         //定义赋值终点下标
         int top = nums.length - 1 ;
         //for loop
+        //终点前一个下标
+        //跳出条件为 i为负数时
+        //i--
         for (int i = nums.length - 2; i >= 0; i--) {
             if (nums[i] + i >= top) top = i;
         }
