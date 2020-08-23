288f8b6d9b95b5fa3407e3ad9f61ab1fe83e26a2
manchungkali <cmkangsong@126.com>
2020-08-23 18:18:36 +0800

    “”

--- Week_07/friend-circles.py
+++ Week_07/friend-circles.py
@@ -66,4 +66,18 @@ class Solution3:
                 if M[a][b]: union(a,b)
         for i in range(len(M)): find(i)
         return len(set(father))
-
+class Solution4:
+    def findCircleNum(self, M) -> int:
+        father = [i for i in range(len(M))]
+        def find(a):
+            if father[a] != a:
+                father[a] = find(father[a])
+            return father[a]
+        def union(a,b):
+            father[find(b)] = find(a)
+            return find(b)
+        for a in range(len(M)):
+            for b in range(a):
+                if M[a][b]: union(a,b)
+        for i in range(len(M)): find(i)
+        return len(set(father))
