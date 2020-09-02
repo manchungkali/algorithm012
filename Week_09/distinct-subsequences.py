3c8734112e23df556a1f6c82b0ead7f0319adf7e
manchungkali <cmkangsong@126.com>
2020-09-02 21:17:09 +0800

    homework09

---
+++ Week_09/distinct-subsequences.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        N1 = len(s)
+        N2 = len(t)
+        if N1 < N2:
+            return 0
+        count = collections.defalutdict(list)
+        for i, c in enumerate(t):
+            count[c].append(i)
+        status = [1] + [0] * N2
+        for ch in s:
+            for j in count[ch][::-1]:
+                status[j + 1] += status[j]
+        return status[-1]
